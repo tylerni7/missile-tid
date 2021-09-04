bb61dde712876c916a1a5d1afeee1ee67fd12186
Thomas A Caswell <tcaswell@gmail.com>
2021-09-04 16:08:15 -0400

    BLD: make setup.py work

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup, Extension
 import os
 
 setup(
-    name="tusa",
+    name="tid",
     version="0.1",
     author="Tyler Nighswander, Michael Nute",
     install_requires=["laika", "matplotlib", "numpy", "scipy"],
-    packages=["tusa"]
+    packages=["tid"],
     description="Ionospheric measurements from GPS",
 )
