cc736a3ffacb59faa2ef7a397c1a7146e86551cf
Thomas A Caswell <tcaswell@gmail.com>
2021-09-04 18:19:42 -0400

    BLD: point at default branch of laika

--- setup.py
+++ setup.py
@@ -5,7 +5,12 @@ setup(
     name="tid",
     version="0.1",
     author="Tyler Nighswander, Michael Nute",
-    install_requires=["laika", "matplotlib", "numpy", "scipy"],
+    install_requires=[
+        "laika @ git+https://github.com/commaai/laika#egg=laika",
+        "matplotlib",
+        "numpy",
+        "scipy",
+    ],
     packages=["tid"],
     description="Ionospheric measurements from GPS",
 )
