0b1fb7aa67bfe129e5645e61f2f6f4cdcfddbc50
Thomas A Caswell <tcaswell@gmail.com>
2021-09-04 19:03:25 -0400

    ENH: make config Path aware

--- tid/config.py
+++ tid/config.py
@@ -4,6 +4,8 @@ Reads shared config and makes object available for use by other modules
 """
 import os
 import yaml
+from pathlib import Path
+from typeing import Union
 
 from ._errors import TidRuntimeError
 
@@ -18,12 +20,17 @@ _GLOBAL_CONFIG = None
 class Configuration:
     """Actual configuration object."""
 
-    def __init__(self, config_file: str = default_config) -> None:
+    def __init__(self, config_file: Union[str, os.PathLike] = default_config) -> None:
+        """
+        Parameters
+        ----------
+        config_file : str or
+        """
         self.config_file = config_file
 
         with open(self.config_file, encoding="utf-8") as fname:
             self.conf = yaml.safe_load(fname)
-            self.cache_dir = os.path.expanduser(self.conf.get("cache_dir"))
+            self.cache_dir = Path(self.conf["cache_dir"]).expanduser()
 
 
 def set_global_config(config: Configuration) -> None:
